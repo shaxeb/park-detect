f1965a195cc16e8375debf1ae1ca60ad0cd53a23
Mohammed Shahzeb <mdshahzeb@live.in>
2023-06-01 12:01:43 +0530

    added comments

--- app.py
+++ app.py
@@ -2,47 +2,61 @@ import cv2
 import numpy as np
 import pickle
 
-cap = cv2.VideoCapture("carpark.mp4")
+cap = cv2.VideoCapture("carpark.mp4")  # Open the video file for capturing frames
 
 try:
     with open("polygon_points.pkl", 'rb') as f:
-        polygons = pickle.load(f)
+        polygons = pickle.load(f)  # Load previously saved polygons from file
 except:
-    polygons = []
+    polygons = []  # If file doesn't exist or failed to load, start with an empty list of polygons
 
-points = []
-drawing_polygon = False
+points = []  # Stores the points of the current polygon being drawn
+drawing_polygon = False  # Flag to indicate if a polygon is being drawn or not
 
 def draw_polygons(frame, polygons):
+    """Draws the stored polygons on the frame"""
     for polygon in polygons:
         points_array = np.array(polygon, np.int32).reshape((-1, 1, 2))
         frame = cv2.polylines(frame, [points_array], isClosed=True, color=(0, 255, 0), thickness=2)
     return frame
 
 def save_polygons(polygons):
+    """Saves the polygons to a file"""
     with open("polygon_points.pkl", 'wb') as f:
         pickle.dump(polygons, f)
 
 def complete_polygon():
+    """Completes the current polygon being drawn and adds it to the list of polygons"""
     global points, drawing_polygon
-    if drawing_polygon and len(points) >= 3:
+    if drawing_polygon and len(points) >= 3:  # Minimum 3 points required to form a polygon
         polygons.append(points)
         points = []
         drawing_polygon = False
 
+def delete_last_polygon():
+    """Deletes the last drawn polygon from the list of polygons"""
+    if len(polygons) > 0:
+        polygons.pop()
+
+def delete_all_polygons():
+    """Deletes all polygons from the list"""
+    polygons.clear()
+
 def mouse_callback(event, x, y, flags, param):
+    """Callback function for mouse events"""
     global points, drawing_polygon
 
     if event == cv2.EVENT_LBUTTONDOWN:
         if not drawing_polygon:
-            points = []
+            points = []  # Start a new polygon
             drawing_polygon = True
-        points.append((x, y))
+        points.append((x, y))  # Add the clicked point to the current polygon
     elif event == cv2.EVENT_RBUTTONDOWN:
         if drawing_polygon:
-            points = []
+            points = []  # Cancel the current polygon if right-clicked
             drawing_polygon = False
         else:
+            # Check if right-clicked point is inside any existing polygon, and remove that polygon
             for i, polygon in enumerate(polygons):
                 if cv2.pointPolygonTest(np.array(polygon), (x, y), False) >= 0:
                     polygons.pop(i)
@@ -53,26 +67,31 @@ while True:
     if not success:
         break
 
-    frame = cv2.resize(frame, (720, 480))
+    frame = cv2.resize(frame, (720, 480))  # Resize the frame for better display
 
-    frame = draw_polygons(frame, polygons)
+    frame = draw_polygons(frame, polygons)  # Draw the stored polygons on the frame
 
     if drawing_polygon and len(points) > 0:
         cv2.polylines(frame, [np.array(points)], isClosed=False, color=(0, 255, 0), thickness=2)
+        # Draw the current polygon being drawn on the frame
 
-    cv2.imshow("Frame", frame)
+    cv2.imshow("Frame", frame)  # Display the frame
 
-    cv2.setMouseCallback("Frame", mouse_callback)
+    cv2.setMouseCallback("Frame", mouse_callback)  # Set the mouse callback function for the frame
 
     key = cv2.waitKey(1) & 0xFF
 
-    if key == 27:  # Press 'Escape' to quit
+    if key == 27:  # Press 'Escape' to quit the program
         break
     elif key == ord("s"):
         save_polygons(polygons)
-        print("Polygon points saved to 'polygon_points.pkl'")
+        print("Polygon points saved to 'polygon_points.pkl'")  # Save the polygons to a file
     elif key == ord("c"):
-        complete_polygon()
+        complete_polygon()  # Complete the current polygon being drawn
+    elif key == ord("d"):
+        delete_last_polygon()  # Delete the last drawn polygon
+    elif key == ord("a"):
+        delete_all_polygons()  # Delete all polygons
 
 cap.release()
 cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -3,34 +3,35 @@ import pickle
 import torch
 import numpy as np
 
-points = []
-
+points = []  # A list to store points
 
 def POINTS(event, x, y, flags, param):
+    """Callback function for mouse events"""
     if event == cv2.EVENT_MOUSEMOVE:
-        colorsBGR = [x, y]
+        colorsBGR = [x, y]  # Get the coordinates of the mouse cursor
         print(colorsBGR)
 
-
 cv2.namedWindow('FRAME')
-cv2.setMouseCallback('FRAME', POINTS)
+cv2.setMouseCallback('FRAME', POINTS)  # Set the mouse callback function for the frame
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)  # Load the YOLOv5 model
 
-cap = cv2.VideoCapture('D:\source-code\image-processing\park-detect\carpark.mp4')
+cap = cv2.VideoCapture('carpark.mp4')  # Open the video file for capturing frames
 count = 0
 
 with open("polygon_points.pkl", 'rb') as f:
-    areas = pickle.load(f)
+    areas = pickle.load(f)  # Load the stored areas from file
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read()  # Read a frame from the video
     if not ret:
         break
-    frame = cv2.resize(frame, (720, 480))
 
-    results = model(frame)
-    list = []
+    frame = cv2.resize(frame, (720, 480))  # Resize the frame for better display
+
+    results = model(frame)  # Perform object detection on the frame using YOLOv5
+
+    list = []  # A list to store objects detected within the defined areas
     for index, row in results.pandas().xyxy[0].iterrows():
         x1 = int(row['xmin'])
         y1 = int(row['ymin'])
@@ -43,20 +44,21 @@ while True:
             for area in areas:
                 results = cv2.pointPolygonTest(np.array(area, np.int32), ((cx, cy)), False)
                 if results >= 0:
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
-                    cv2.putText(frame, str(d), (x1, y1), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 3)  # Draw bounding box around the car
+                    cv2.putText(frame, str(d), (x1, y1), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)  # Add label
                     list.append([cx])
 
     for area in areas:
-        cv2.polylines(frame, [np.array(area, np.int32)], True, (0, 255, 0), 2)
+        cv2.polylines(frame, [np.array(area, np.int32)], True, (0, 255, 0), 2)  # Draw the defined areas
 
-    a = len(list)
-    cv2.putText(frame, str(a), (82, 69), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-    cv2.imshow("FRAME", frame)
-    cv2.setMouseCallback("FRAME", POINTS)
+    a = len(list)  # Count the number of cars detected within the defined areas
+    cv2.putText(frame, str(a), (82, 69), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)  # Add car count to the frame
 
-    if cv2.waitKey(1) & 0xFF == 27:
+    cv2.imshow("FRAME", frame)  # Display the frame
+    cv2.setMouseCallback("FRAME", POINTS)  # Set the mouse callback function for the frame
+
+    if cv2.waitKey(1) & 0xFF == 27:  # Press 'Escape' to quit the program
         break
 
-cap.release()
-cv2.destroyAllWindows()
+cap.release()  # Release the video capture
+cv2.destroyAllWindows()  # Close all windows
