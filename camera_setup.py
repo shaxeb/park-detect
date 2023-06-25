bfed783a3ecfc09ca76360a366296d9c7b1c7b96
Mohammed Shahzeb <mdshahzeb@live.in>
2023-06-25 17:06:26 +0530

    added multiple camera input

--- camera_setup.py
+++ camera_setup.py
@@ -1,45 +1,7 @@
 import json
-
 import cv2
+import sys
 import numpy as np
-
-# Prompt the user to select an option
-print('Select your option:')
-print('Enter 1 for loading previously used camera IP, polygons, and labels')
-print('Enter 2 for entering a new camera IP \n')
-ip_select = int(input())
-
-if ip_select == 1:
-    try:
-        with open("assets/data.json", 'r') as f:
-            data = json.load(f)  # Load previously saved data from file
-            # Convert label keys to integers
-            data["labels"] = {int(key): value for key,
-                              value in data["labels"].items()}
-    except:
-        # If file doesn't exist or failed to load, start with empty data
-        data = {"camera_ip": "", "polygons": [], "labels": {}}
-        print("There's no saved camera IP, Enter a new IP: ")
-        data["camera_ip"] = str(input())
-        with open("assets/data.json", 'w') as f:
-            json.dump(data, f)
-elif ip_select == 2:
-    print("Enter a new IP: ")
-    camera_ip = str(input())
-    data = {"camera_ip": camera_ip, "polygons": [], "labels": {}}
-    with open("assets/data.json", 'w') as f:
-        json.dump(data, f)
-
-print('Draw a polygon using your mouse and then label it')
-# Open the video stream for capturing frames
-cap = cv2.VideoCapture(data["camera_ip"])
-
-polygons = data["polygons"]  # Stores the drawn polygons
-labels = data["labels"]  # Stores the labels for each polygon
-
-points = []  # Stores the points of the current polygon being drawn
-drawing_polygon = False  # Flag to indicate if a polygon is being drawn or not
-
 
 def draw_polygons(frame, polygons, labels):
     """Draws the stored polygons and labels on the frame"""
@@ -48,19 +10,17 @@ def draw_polygons(frame, polygons, labels):
         frame = cv2.polylines(
             frame, [points_array], isClosed=True, color=(0, 255, 0), thickness=2)
         # Check if i exists as a key and the value is not empty
-        if i in labels and labels[i]:
-            label = labels[i]
+        if str(i) in labels and labels[str(i)]:
+            label = labels[str(i)]
             label_position = (polygon[0][0], polygon[0][1] - 10)
             cv2.putText(frame, label, label_position,
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
     return frame
 
-
 def save_data(data):
     """Saves the data to a file"""
     with open("assets/data.json", 'w') as f:
         json.dump(data, f)
-
 
 def complete_polygon(label):
     """Completes the current polygon being drawn, adds it to the list of polygons, and saves the label"""
@@ -72,19 +32,16 @@ def complete_polygon(label):
         points = []
         drawing_polygon = False
 
-
 def delete_last_polygon():
     """Deletes the last drawn polygon from the list of polygons"""
     if len(polygons) > 0:
         polygons.pop()
         labels.pop(len(polygons), None)
 
-
 def delete_all_polygons():
     """Deletes all polygons from the list"""
     polygons.clear()
     labels.clear()
-
 
 def mouse_callback(event, x, y, flags, param):
     """Callback function for mouse events"""
@@ -107,47 +64,206 @@ def mouse_callback(event, x, y, flags, param):
                     labels.pop(i, None)
                     break
 
+try:
+    with open("assets/data.json", 'r') as f:
+        data = json.load(f)
+        # Remove unused "polygons" and "labels"
+        data.pop("polygons", None)
+        data.pop("labels", None)
+        # Convert label keys to integers
+        data["cameras"] = {
+            camera_name: {
+                "camera_ip": camera_data["camera_ip"],
+                "polygons": camera_data.get("polygons", []),
+                "labels": {str(key): value for key, value in camera_data.get("labels", {}).items()}
+            }
+            for camera_name, camera_data in data["cameras"].items()
+        }
 
-while True:
-    success, frame = cap.read()
-    if not success:
-        break
+except (FileNotFoundError, json.JSONDecodeError):
+    # If file doesn't exist or failed to load, start with empty data
+    data = {"cameras": {}}
 
-    # Resize the frame for better display
-    frame = cv2.resize(frame, (720, 480))
+# Prompt the user to select an option
+print('Select your option:')
+print('Enter 1 to load previously used cameras, polygons, and labels')
+print('Enter 2 to add a new camera and annotate polygons')
+option = int(input())
 
-    # Draw the stored polygons and labels on the frame
-    frame = draw_polygons(frame, polygons, labels)
+if option == 1:
+    if not data["cameras"]:
+        print("No cameras available. Add a new camera to annotate.")
+        option = 2
 
-    if drawing_polygon and len(points) > 0:
-        cv2.polylines(frame, [np.array(points)],
-                      isClosed=False, color=(0, 255, 0), thickness=2)
-        # Draw the current polygon being drawn on the frame
+if option == 2:
+    print("Enter a name for the new camera:")
+    camera_name = str(input())
+    print("Enter the IP address of the new camera:")
+    camera_ip = str(input())
 
-    cv2.imshow("Frame", frame)  # Display the frame
+    # Add the new camera to the data
+    data["cameras"][camera_name] = {
+        "camera_ip": camera_ip,
+        "polygons": [],
+        "labels": {}
+    }
 
-    # Set the mouse callback function for the frame
-    cv2.setMouseCallback("Frame", mouse_callback)
+    # Save the updated data to the file
+    with open("assets/data.json", 'w') as f:
+        json.dump(data, f)
 
-    key = cv2.waitKey(1) & 0xFF
+    print("New camera added successfully!")
 
-    if key == 27:  # Press 'Escape' to quit the program
-        break
-    elif key == ord("s"):
-        # Prompt the user to enter a label
-        label = input("Enter a label for the polygon: ")
-        # Complete the current polygon being drawn and save the label
-        complete_polygon(label)
-        save_data({"camera_ip": data["camera_ip"],
-                  "polygons": polygons, "labels": labels})
-    elif key == ord("d"):
-        delete_last_polygon()  # Delete the last drawn polygon
-        save_data({"camera_ip": data["camera_ip"],
-                  "polygons": polygons, "labels": labels})
-    elif key == ord("x"):
-        delete_all_polygons()  # Delete all polygons
-        save_data({"camera_ip": data["camera_ip"],
-                  "polygons": polygons, "labels": labels})
+    print("Select an option:")
+    print("Enter 1 to annotate the newly added camera")
+    print("Enter 2 to exit")
 
-cap.release()
-cv2.destroyAllWindows()
+    annotation_option = int(input())
+
+    if annotation_option == 1:
+        # Load the newly added camera for annotation
+        selected_camera_name = camera_name
+        selected_camera = data["cameras"][selected_camera_name]
+
+        # Load camera IP and polygons for annotation
+        camera_ip = selected_camera["camera_ip"]
+        polygons = selected_camera["polygons"]
+        labels = selected_camera["labels"]
+
+        print('Draw a polygon using your mouse and then label it')
+        cap = cv2.VideoCapture(camera_ip)
+
+        points = []  # Stores the points of the current polygon being drawn
+        drawing_polygon = False  # Flag to indicate if a polygon is being drawn or not
+
+        while True:
+            success, frame = cap.read()
+            if not success:
+                break
+
+            # Resize the frame for better display
+            frame = cv2.resize(frame, (720, 480))
+
+            # Draw the stored polygons and labels on the frame
+            frame = draw_polygons(frame, polygons)
+
+            # Draw the labels on the frame
+            for label, points in labels.items():
+                for point in points:
+                    cv2.putText(frame, label, tuple(point), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+
+            if drawing_polygon and len(points) > 0:
+                cv2.polylines(frame, [np.array(points)],
+                              isClosed=False, color=(0, 255, 0), thickness=2)
+                # Draw the current polygon being drawn on the frame
+
+            cv2.imshow("Frame", frame)  # Display the frame
+
+            # Set the mouse callback function for the frame
+            cv2.setMouseCallback("Frame", mouse_callback)
+
+            key = cv2.waitKey(1) & 0xFF
+
+            if key == 27:  # Press 'Escape' to quit the program
+                break
+            elif key == ord("s"):
+                # Prompt the user to enter a label
+                label = input("Enter a label for the polygon: ")
+                # Complete the current polygon being drawn and save the label
+                complete_polygon(label)
+                # Save the data for the selected camera
+                selected_camera["polygons"] = polygons
+                selected_camera["labels"] = labels
+                save_data(data)
+            elif key == ord("d"):
+                delete_last_polygon()  # Delete the last drawn polygon
+                # Save the data for the selected camera
+                selected_camera["polygons"] = polygons
+                selected_camera["labels"] = labels
+                save_data(data)
+            elif key == ord("x"):
+                delete_all_polygons()  # Delete all polygons
+                # Save the data for the selected camera
+                selected_camera["polygons"] = polygons
+                selected_camera["labels"] = labels
+                save_data(data)
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+    elif annotation_option == 2:
+        print("Exiting the program.")
+        sys.exit(0)
+
+
+if option == 1:
+    print("Select a camera to annotate:")
+    camera_names = list(data["cameras"].keys())
+    for i, name in enumerate(camera_names):
+        print(f"Enter {i+1} for {name}")
+
+    camera_select = int(input())
+    selected_camera_name = camera_names[camera_select - 1]
+    selected_camera = data["cameras"][selected_camera_name]
+
+    # Load camera IP and polygons for annotation
+    camera_ip = selected_camera["camera_ip"]
+    polygons = selected_camera["polygons"]
+    labels = selected_camera["labels"]
+
+    print('Draw a polygon using your mouse and then label it')
+    cap = cv2.VideoCapture(camera_ip)
+
+    points = []  # Stores the points of the current polygon being drawn
+    drawing_polygon = False  # Flag to indicate if a polygon is being drawn or not
+
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        # Resize the frame for better display
+        frame = cv2.resize(frame, (720, 480))
+
+        # Draw the stored polygons and labels on the frame
+        frame = draw_polygons(frame, polygons, labels)
+
+        if drawing_polygon and len(points) > 0:
+            cv2.polylines(frame, [np.array(points)],
+                          isClosed=False, color=(0, 255, 0), thickness=2)
+            # Draw the current polygon being drawn on the frame
+
+        cv2.imshow("Frame", frame)  # Display the frame
+
+        # Set the mouse callback function for the frame
+        cv2.setMouseCallback("Frame", mouse_callback)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == 27:  # Press 'Escape' to quit the program
+            break
+        elif key == ord("s"):
+            # Prompt the user to enter a label
+            label = input("Enter a label for the polygon: ")
+            # Complete the current polygon being drawn and save the label
+            complete_polygon(label)
+            # Save the data for the selected camera
+            selected_camera["polygons"] = polygons
+            selected_camera["labels"] = labels
+            save_data(data)
+        elif key == ord("d"):
+            delete_last_polygon()  # Delete the last drawn polygon
+            # Save the data for the selected camera
+            selected_camera["polygons"] = polygons
+            selected_camera["labels"] = labels
+            save_data(data)
+        elif key == ord("x"):
+            delete_all_polygons()  # Delete all polygons
+            # Save the data for the selected camera
+            selected_camera["polygons"] = polygons
+            selected_camera["labels"] = labels
+            save_data(data)
+
+    cap.release()
+    cv2.destroyAllWindows()
--- detect.py
+++ detect.py
@@ -3,34 +3,41 @@ import cv2
 import numpy as np
 import torch
 
-points = []  # A list to store points
-
-with open("assets/data.json", 'rb') as f:
-    data = json.load(f)  # Load camera IP, polygons, and labels from file
-
-camera_ip = data["camera_ip"]
-polygons = data["polygons"]
-labels = data["labels"]
-# Convert the keys to integers
-labels = {int(k): v for k, v in data["labels"].items()}
-
-
-def POINTS(event, x, y, flags, param):
+def mouse_callback(event, x, y, flags, param):
     """Callback function for mouse events"""
     if event == cv2.EVENT_MOUSEMOVE:
         colorsBGR = [x, y]  # Get the coordinates of the mouse cursor
         print(colorsBGR)
 
+# Load camera IP, polygons, and labels from file
+with open("assets/data.json", 'r') as f:
+    data = json.load(f)
+
+camera_names = list(data["cameras"].keys())
+if len(camera_names) == 0:
+    print("No cameras available. Please add a camera to the JSON database.")
+    exit()
+
+print("Select a camera:")
+for i, name in enumerate(camera_names):
+    print(f"Enter {i+1} for {name}")
+
+camera_select = int(input())
+selected_camera_name = camera_names[camera_select - 1]
+selected_camera = data["cameras"][selected_camera_name]
+camera_ip = selected_camera["camera_ip"]
+polygons = selected_camera["polygons"]
+labels = selected_camera["labels"]
+
+# Convert the label keys to integers
+labels = {int(k): v for k, v in labels.items()}
 
 cv2.namedWindow('FRAME')
-# Set the mouse callback function for the frame
-cv2.setMouseCallback('FRAME', POINTS)
+cv2.setMouseCallback('FRAME', mouse_callback)
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s',
-                       pretrained=True)  # Load the YOLOv5 model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)  # Load the YOLOv5 model
 
 cap = cv2.VideoCapture(camera_ip)  # Open the video stream for capturing frames
-count = 0
 
 while True:
     ret, frame = cap.read()  # Read a frame from the video
@@ -43,40 +50,32 @@ while True:
     # Perform object detection on the frame using YOLOv5
     results = model(frame)
 
-    list = []  # A list to store objects detected within the defined areas
+    car_count = 0
     for index, row in results.pandas().xyxy[0].iterrows():
         x1 = int(row['xmin'])
         y1 = int(row['ymin'])
         x2 = int(row['xmax'])
         y2 = int(row['ymax'])
-        d = (row['name'])
-        cx = int(x1 + x2) // 2
-        cy = int(y1 + y2) // 2
+        d = row['name']
+        cx = (x1 + x2) // 2
+        cy = (y1 + y2) // 2
         if 'car' in d:
             for area in polygons:
-                results = cv2.pointPolygonTest(
-                    np.array(area, np.int32), (cx, cy), False)
-                if results >= 0:
+                if cv2.pointPolygonTest(np.array(area, np.int32), (cx, cy), False) >= 0:
                     # Draw bounding box around the car
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
-                    # cv2.putText(frame, str(d), (x1, y1), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)  # Add label
-                    list.append([cx])
+                    car_count += 1
 
     for i, area in enumerate(polygons):
-        cv2.polylines(frame, [np.array(area, np.int32)],
-                      True, (0, 255, 0), 2)  # Draw the defined areas
+        cv2.polylines(frame, [np.array(area, np.int32)], True, (0, 255, 0), 2)  # Draw the defined areas
         if i in labels:
             label = labels[i]
             label_position = (area[0][0], area[0][1] - 10)
-            cv2.putText(frame, label, label_position,
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
 
-    a = len(list)  # Count the number of cars detected within the defined areas
-    cv2.putText(frame, str(a), (82, 69), cv2.FONT_HERSHEY_PLAIN,
-                2, (255, 0, 0), 2)  # Add car count to the frame
+    cv2.putText(frame, str(car_count), (82, 69), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)  # Add car count to the frame
 
     cv2.imshow("FRAME", frame)  # Display the frame
-    # cv2.setMouseCallback("FRAME", POINTS)  # Set the mouse callback function for the frame
 
     if cv2.waitKey(1) & 0xFF == 27:  # Press 'Escape' to quit the program
         break
