ff34f6e0c59174b6f7b6bfbefddc5e2a75e2d376
Mohammed Shahzeb <mdshahzeb@live.in>
2023-07-05 03:48:23 +0530

    .

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 import sys
-from typing import Optional
 from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton
-from PySide6.QtCore import Slot, QFile, QTextStream
-
+from PySide6.QtCore import Slot
 from ui_app import Ui_MainWindow
+
+class Model:
+    def __init__(self):
+        self.current_index = 0
+
+class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+        self.view.home_btn_1.toggled.connect(lambda: self.on_button_toggled(0))
+        self.view.home_btn_2.toggled.connect(lambda: self.on_button_toggled(0))
+        self.view.cam_btn_1.toggled.connect(lambda: self.on_button_toggled(1))
+        self.view.cam_btn_2.toggled.connect(lambda: self.on_button_toggled(1))
+        self.view.edit_btn_1.toggled.connect(lambda: self.on_button_toggled(2))
+        self.view.edit_btn_2.toggled.connect(lambda: self.on_button_toggled(2))
+
+        self.view.user_btn.clicked.connect(self.user_btn_clicked)
+        self.view.search_btn.clicked.connect(self.search_btn_clicked)
+
+    @Slot()
+    def search_btn_clicked(self):
+        self.view.stackedWidget.setCurrentIndex(4)
+        search_text = self.view.search_input.text().strip()
+        if search_text:
+            self.view.search_string_label.setText(search_text)
+
+    @Slot()
+    def user_btn_clicked(self):
+        self.view.stackedWidget.setCurrentIndex(3)
+
+    @Slot(int)
+    def on_button_toggled(self, index):
+        if index >= 0 and index < self.view.stackedWidget.count():
+            self.view.stackedWidget.setCurrentIndex(index)
+            self.model.current_index = index
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -16,52 +50,30 @@ class MainWindow(QMainWindow):
         self.ui.stackedWidget.setCurrentIndex(0)
         self.ui.home_btn_2.setChecked(True)
 
-        self.ui.home_btn_1.toggled.connect(lambda: self.on_button_toggled(0))
-        self.ui.home_btn_2.toggled.connect(lambda: self.on_button_toggled(0))
-        self.ui.cam_btn_1.toggled.connect(lambda: self.on_button_toggled(1))
-        self.ui.cam_btn_2.toggled.connect(lambda: self.on_button_toggled(1))
-        self.ui.edit_btn_1.toggled.connect(lambda: self.on_button_toggled(2))
-        self.ui.edit_btn_2.toggled.connect(lambda: self.on_button_toggled(2))
+        self.model = Model()
+        self.controller = Controller(self.model, self.ui)
 
-        self.ui.user_btn.clicked.connect(self.user_btn_clicked)
-        self.ui.search_btn.clicked.connect(self.search_btn_clicked)
+        self.model.current_index = 0
+        self.ui.stackedWidget.setCurrentIndex(self.model.current_index)
 
-    #functions to searching
-    def search_btn_clicked(self):
-        self.ui.stackedWidget.setCurrentIndex(4)
-        search_text = self.ui.search_input.text().strip()
-        if search_text:
-            self.ui.search_string_label.setText(search_text)
+        self.ui.stackedWidget.currentChanged.connect(self.on_stackedWidget_currentChanged)
 
-    #function for changing to user page
-    def user_btn_clicked(self):
-        self.ui.stackedWidget.setCurrentIndex(3)
-    
-    #change QPushButton Checkable status when stackedWidget index changed 
+    @Slot(int)
     def on_stackedWidget_currentChanged(self, index):
         btn_list = self.ui.icon_only_widget.findChildren(QPushButton) \
-                    + self.ui.full_menu_widget.findChildren(QPushButton)
+                   + self.ui.full_menu_widget.findChildren(QPushButton)
         
         for btn in btn_list:
-            if index in [3,4]:
+            if index in [3, 4]:
                 btn.setAutoExclusive(False)
                 btn.setChecked(False)
             else:
                 btn.setAutoExclusive(True)
 
-
-    def on_button_toggled(self, index):
-        if index >= 0 and index < self.ui.stackedWidget.count():
-            self.ui.stackedWidget.setCurrentIndex(index)
-
-
-
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     # loading stylesheet
-
     with open("style.qss", "r") as stylesheet:
         stylesheet = stylesheet.read()
 
